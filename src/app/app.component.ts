6e25e1a5d166289534bbc2accd67d147cda3c69c
Greyson-Sawyer <showmethemonkey2@gmail.com>
2022-06-07 12:29:36 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo: ElementRef<HTMLVideoElement>;
   name = 'Angular';
   images = Array.from(Array(20).keys());
 
   constructor() {}
+
+  ngAfterViewInit() {
+    this.heroVideo.nativeElement.muted = true;
+  }
 }
